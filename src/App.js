68230f7789e67abf41b80872eca6b8f37457ad40
datobobokhia <dato.bobokhia@gmail.com>
2022-08-25 02:41:28 +0400

    body design

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
-import MobileHeader from './mobile/MobileHeader'
-import BodyFrame from './mobile/BodyFrame'
+import MobileHeader from './mobile/MobileHeader';
+import BodyFrame from './mobile/BodyFrame';
+import BodyDesignProject from './mobile/BodyDesignProject';
 
 function App() {
   return (
     <div className="App">
       <MobileHeader/>
       <BodyFrame/>
+      <div className='designPortfolio'>
+        <BodyDesignProject design={'web'}/>
+        <BodyDesignProject design={'app'}/>
+        <BodyDesignProject design={'graphic'}/>
+      </div>
     </div>
   );
 }
