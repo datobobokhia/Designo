67e5e6e927dfb98247dff3308ac3356f095b902c
datobobokhia <dato.bobokhia@gmail.com>
2022-09-11 00:19:57 +0400

    fixed some bugs

--- src/components/MainHomePage/DesignProject.js
+++ src/components/MainHomePage/DesignProject.js
@@ -1,47 +1,57 @@
 import "./DesignProject.css";
-// import { BrowserRouter as Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function WebDesign({ design }) {
+  const getImageSource = () => {
+    if (design === "web") {
+      return "/assets/photo/image-web-design.jpg";
+    } else if (design === "app") {
+      return "/assets/photo/image-app-design.jpg";
+    } else {
+      return "/assets/photo/image-graphic-design.jpg";
+    }
+  };
+
+  const getInnerText = () => {
+    if (design === "web") {
+      return "innerTextWeb";
+    } else if (design === "app") {
+      return "innerTextApp";
+    } else {
+      return "innerTextGraphic";
+    }
+  };
+
+  const getHeadline = () => {
+    if (design === "web") {
+      return "WEB DESIGN";
+    } else if (design === "app") {
+      return "APP DESIGN";
+    } else {
+      return "GRAPHIC DESIGN";
+    }
+  };
+
   return (
     <div className="webDesign">
-      <img
-        src={
-          design === "web"
-            ? "/assets/photo/image-web-design.jpg"
-            : design === "app"
-            ? "/assets/photo/image-app-design.jpg"
-            : "/assets/photo/image-graphic-design.jpg"
-        }
-        alt="Design"
-        className="designImage"
-      />
-      <div
-        className={
-          design === "web"
-            ? "innerTextWeb"
-            : design === "app"
-            ? "innerTextApp"
-            : "innerTextGraphic"
-        }
-      >
-        <div
-          className={design === "graphic" ? "designTypeGraph" : "designType"}
-        >
-          {design === "web"
-            ? "WEB DESIGN"
-            : design === "app"
-            ? "APP DESIGN"
-            : "GRAPHIC DESIGN"}
+      <Link to="webdesign">
+        <img src={getImageSource()} alt="Design" className="designImage" />
+        <div className={getInnerText()}>
+          <div
+            className={design === "graphic" ? "designTypeGraph" : "designType"}
+          >
+            {getHeadline()}
+          </div>
+          <div className="linkText">
+            VIEW PROJECTS
+            <img
+              src="/assets/photo/icon-right-arrow.svg"
+              alt="Arrow Icon"
+              className="arrowIcon"
+            />
+          </div>
         </div>
-        <div className="linkText">
-          VIEW PROJECTS
-          <img
-            src="/assets/photo/icon-right-arrow.svg"
-            alt="Arrow Icon"
-            className="arrowIcon"
-          />
-        </div>
-      </div>
+      </Link>
     </div>
   );
 }
